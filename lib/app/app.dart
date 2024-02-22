3978f910c876a651295ac9c07678d2c929e73ddc
Hassan Abo-El-Hamd <150099731+hassanoshea@users.noreply.github.com>
2024-02-23 00:23:39 +0200

    Update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,24 @@
-
 import 'package:flutter/material.dart';
-import 'package:to_do_app/feature/auth/screens/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_app/feature/task/presentation/cubit/task_cubit.dart';
 
-class ToDoApp extends StatelessWidget {
-  const ToDoApp({super.key});
+import 'app/app.dart';
+import 'core/bloc/bloc_observer.dart';
+import 'core/database/cache/cache_helper.dart';
+import 'core/database/sqflite_helper/sqflite_helper.dart';
+import 'core/services/service_locator.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return  const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen()
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  await setup();
+  await sl<CacheHelper>().init();
+  sl<SqfliteHelper>().intiDB();
+
+  runApp(
+    BlocProvider(
+      create: (context) => TaskCubit()..getTheme()..getTasks(),
+      child: const MyApp(),
+    ),
+  );
 }
