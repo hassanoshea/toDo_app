92c12cee97ab420a97d3f152830a2af219807715
Hassan Abo-El-Hamd <150099731+hassanoshea@users.noreply.github.com>
2024-02-23 00:21:35 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:to_do_app/app/app.dart';
+import 'package:to_do_app/feature/task/presentation/cubit/task_cubit.dart';
 
-void main() {
-  runApp(const ToDoApp());
+import 'app/app.dart';
+import 'core/bloc/bloc_observer.dart';
+import 'core/database/cache/cache_helper.dart';
+import 'core/database/sqflite_helper/sqflite_helper.dart';
+import 'core/services/service_locator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  await setup();
+  await sl<CacheHelper>().init();
+  sl<SqfliteHelper>().intiDB();
+
+  runApp(
+    BlocProvider(
+      create: (context) => TaskCubit()..getTheme()..getTasks(),
+      child: const MyApp(),
+    ),
+  );
 }
-
-
